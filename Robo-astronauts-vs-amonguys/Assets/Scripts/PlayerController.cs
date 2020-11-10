9dde778945a0601332add52795fb97640a163b7c
Gers <Gers2017@outlook.com>
2020-11-10 16:24:53 -0600

    player movement and camera orbit added
    The player now moves using the horizontal and vertical axis.
    The camera orbits around the player and modify its rotation to always track the back of the player.

--- Robo-astronauts-vs-amonguys/Assets/Scripts/PlayerController.cs
+++ Robo-astronauts-vs-amonguys/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController character;
-    [SerializeField] Transform camera_body;
     float speed = 10f;
-    float turn_speed = 0.5f;
+
     void Start()
     {
         character = GetComponent<CharacterController>();
@@ -23,14 +22,8 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
-        float current_angle = transform.eulerAngles.y;
-        float desired_agle = camera_body.eulerAngles.y;
-        float angle_y = Mathf.LerpAngle(desired_agle, current_angle, Time.deltaTime * turn_speed);
-        transform.rotation = Quaternion.Euler(0, desired_agle, 0);
-
-        Vector3 movement = (transform.forward * vertical) + (transform.right * horizontal);
-        character.Move(movement * speed * Time.deltaTime);
         
+        Vector3 movement = (transform.forward * vertical + transform.right * horizontal);
+        character.Move(movement.normalized * speed * Time.deltaTime);
     }
 }
