f9baff4a1a758bfd529498071c26d5863b534127
Gers <Gers2017@outlook.com>
2020-11-10 12:54:35 -0600

    Base project

---
+++ Robo-astronauts-vs-amonguys/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform character_body;
+    float x_angle;
+    Vector3 offset;
+    float damping = 1.5f;
+
+    void Start()
+    {
+        offset = character_body.position - transform.position;
+        x_angle = transform.eulerAngles.x;
+        transform.SetParent(null);
+    }
+
+    void LateUpdate()
+    {
+
+        float current_rotation = transform.eulerAngles.y;
+        float desired_rotation = character_body.eulerAngles.y;
+        float y_angle = Mathf.LerpAngle(current_rotation, desired_rotation, damping * Time.deltaTime);
+
+        //transform.Rotate(Vector3.up, y_angle);
+        Quaternion rotation = Quaternion.Euler(x_angle, y_angle, 0f);
+        transform.rotation = rotation;
+
+        transform.position = character_body.position - (rotation * offset);
+        transform.LookAt(character_body);
+    }
+}
---
+++ Robo-astronauts-vs-amonguys/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController character;
+    [SerializeField] Transform camera_body;
+    float speed = 10f;
+    float turn_speed = 0.5f;
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float current_angle = transform.eulerAngles.y;
+        float desired_agle = camera_body.eulerAngles.y;
+        float angle_y = Mathf.LerpAngle(desired_agle, current_angle, Time.deltaTime * turn_speed);
+        transform.rotation = Quaternion.Euler(0, desired_agle, 0);
+
+        Vector3 movement = (transform.forward * vertical) + (transform.right * horizontal);
+        character.Move(movement * speed * Time.deltaTime);
+        
+    }
+}
