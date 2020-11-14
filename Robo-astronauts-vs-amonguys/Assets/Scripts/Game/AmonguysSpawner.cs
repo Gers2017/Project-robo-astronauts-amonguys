43cd1219bc82499745faa8387416d92921101be1
Gers <Gers2017@outlook.com>
2020-11-13 18:51:12 -0600

    Amonguys movement and Robi animations
    The Amonguys are able to follow the player and respawn.
    Amonguys spawner added.
    Robi with all of its animations was added.
    Rock textures were added to the floor.

---
+++ Robo-astronauts-vs-amonguys/Assets/Scripts/Game/AmonguysSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AmonguysSpawner : MonoBehaviour
+{
+    ObjectPooler pooler;
+    public float spawn_time = 5f;
+    public int max_instances = 10;
+    void Start()
+    {
+        pooler = GetComponent<ObjectPooler>();
+        InvokeRepeating("SpawnAmonguy", spawn_time, spawn_time);
+    }
+    
+    void SpawnAmonguy()
+    {
+        if(pooler.GetActiveInstances() >= max_instances)
+        {
+            return;
+        }
+
+        Vector3 spawn_pos = transform.position + 
+        new Vector3(Random.value * 20f, 0f, Random.value * 20f);
+
+        GameObject instance = pooler.GetInstance(spawn_pos);
+        
+        Amonguys amonguys = instance.GetComponent<Amonguys>();
+        amonguys?.Revive();
+    }
+}
